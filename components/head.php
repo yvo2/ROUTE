4abeee4dfdcbe923f1cb80179ea201e8500baa21
yvo2 <yvo.a.hofer@bluewin.ch>
2017-10-16 22:03:09 +0200

    added Bootstrap

--- components/head.php
+++ components/head.php
@@ -10,4 +10,11 @@ if (isset($title) && isset($page)) {
   <meta charset="utf-8">
   <title><?php echo $title; ?></title>
   <link href="https://fonts.googleapis.com/css?family=Arimo" rel="stylesheet">
+  <link href="style/style.css" rel="stylesheet">
+  <link href="style/dist/bootstrap.min.css" rel="stylesheet">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
 </head>
