e49908adb05e7c6e97af830c364b2a252abe27b9
Bärtschi Lars <bbaerl@bbc-be.local>
2017-10-17 15:50:23 +0200

    + Added users and database

--- config/config.php
+++ config/config.php
@@ -5,9 +5,9 @@ $config = array(
   "endpoint_url"  => 'http://transport.opendata.ch/v1/connections',
 
   "database" => array(
-    "host"      => 'localhost:3007',
+    "host"      => 'localhost:3307',
     "username"  => 'root',
-    "password"  => 'password',
+    "password"  => '',
     "database"  => 'route-db'
   )
 );
