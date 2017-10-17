4856944c13d051f95eaa8acf6a6321ba9a1273b7
Bärtschi Lars <bbaerl@bbc-be.local>
2017-10-17 16:56:49 +0200

    + Adapted links in footer to open in new tab

--- view/footer.php
+++ view/footer.php
@@ -6,7 +6,7 @@
       <!--<p class="float-right">
         <a href="#">Back to top</a>
       </p>-->
-      <p>&copy; by Yvo Hofer and Lars Bärtschi. Powered by <a href="http://transport.opendata.ch">OpenData.ch</a>.<br> Diese Seite steht in keiner Beziehung mit der Schweizerischen Bundesbahn (SBB) oder einem anderen Transportunternehmen. <br><a href="/impressum">Impressum</a></p>
+      <p>&copy; by Yvo Hofer and Lars Bärtschi. Powered by <a target="_blank" href="http://transport.opendata.ch">OpenData.ch</a>.<br> Diese Seite steht in keiner Beziehung mit der Schweizerischen Bundesbahn (SBB) oder einem anderen Transportunternehmen. <br><a href="/impressum" target="_blank">Impressum</a></p>
     </div>
   </footer>
 </body>
