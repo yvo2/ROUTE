116b5148657f46d75f72066312d9b8fbad23bbae
yvo2 <bhofey@bbc-be.local>
2017-10-17 15:19:28 +0200

    changed names

--- view/default_index.php
+++ view/default_index.php
@@ -4,7 +4,7 @@
     <p class="lead text-muted">Finde eine Exkursion!</p>
     <form method="post" class="rt-search">
       <label class="text-muted" for="rt-homebase">Homebase: </label><input value="<?= $homebase ?>" placeholder="z.B. Bern" class="form-control" id="rt-homebase" name="rt-homebase" type="text"><br>
-      <label class="text-muted" for="rt-via">Was möchtest du entdecken? </label><input value="<?= $via ?>" placeholder="z.B. Locarno" class="form-control" id="rt-via" name="rt-via" type="text"><br>
+      <label class="text-muted" for="rt-via">Welche Destination möchtest du besuchen? </label><input value="<?= $via ?>" placeholder="z.B. Locarno" class="form-control" id="rt-via" name="rt-via" type="text"><br>
       <input class="btn btn-primary"type="submit" value="Los gehts!">
     </form>
   </div>
@@ -15,7 +15,7 @@
     <?php
       if (strlen($homebase) != 0 && strlen($via) != 0) {
         ?>
-          Von <?= $homebase ?> nach <?= $via ?> und zurück.
+          Von <?= $homebase ?> nach <?= $via ?> und zurück nach <?= $homebase ?>.
           <hr>
         <?php
         if (!$connectionResponse->hasConnections()) {
