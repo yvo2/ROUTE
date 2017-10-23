589cc955a223bc80ef800c27cee4ad80abd46e44
Bärtschi Lars <bbaerl@bbc-be.local>
2017-10-23 12:58:18 +0200

    - Cleaned up UserController

--- controller/UserController.php
+++ controller/UserController.php
@@ -104,31 +104,4 @@ class UserController extends Controller {
       header("Location: /");
       die('<a href="/">Weiter.</a>');
     }
-
-    public function create() {
-        $view = new View('user_create');
-        $view->title = 'Benutzer erstellen';
-        $view->heading = 'Benutzer erstellen';
-        $view->display();
-    }
-
-    public function doCreate() {
-        if ($_POST['send']) {
-            $firstName = $_POST['firstName'];
-            $lastName = $_POST['lastName'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-            $userRepository = new UserRepository();
-            $userRepository->create($firstName, $lastName, $email, $password);
-        }
-        // Anfrage an die URI /user weiterleiten (HTTP 302)
-        header('Location: /user');
-    }
-
-    public function delete() {
-        $userRepository = new UserRepository();
-        $userRepository->deleteById($_GET['id']);
-        // Anfrage an die URI /user weiterleiten (HTTP 302)
-        header('Location: /user');
-    }
 }
