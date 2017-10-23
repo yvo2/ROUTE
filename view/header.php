9719a205fd9d55209ff83683148f47633ae97d08
yvo2 <bhofey@bbc-be.local>
2017-10-23 09:35:35 +0200

    + New Authentification Buttons / Logged on Button

--- view/header.php
+++ view/header.php
@@ -15,10 +15,16 @@
     <div class="navbar navbar-dark">
       <div class="container d-flex justify-content-between">
         <a href="/" class="navbar-brand">ROUTE</a>
+        <div class="form-inline my-2 my-lg-0">
+          <?php if ($user->signedIn) { ?>
+          <a href="/User" class="text-white"><?= "Hallo, " . $user->email . "." ?></a>
+        <?php } else { ?>
+          <a href="/User/register" class="text-white textmargin">Registrieren</a>
+          <a href="/User/login" class="text-white">Anmelden</a>
+        <?php } ?>
+        </div>
       </div>
-      <div class="container d-flex pull-right justify-content-between">
-        <a href="/User"><?= $user->email ?></a>
-      </div>
+
     </div>
 
     <div class="album text-muted">
