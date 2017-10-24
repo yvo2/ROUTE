5d40ed5b0e98c1925264e025bee46f6edb74af51
yvo2 <bhofey@bbc-be.local>
2017-10-24 12:06:50 +0200

    typo

--- view/user_login.php
+++ view/user_login.php
@@ -2,7 +2,7 @@
   <h1 class="jumbotron-heading">EINLOGGEN</h1>
   <p class="lead text-muted">Melde dich an. Noch keinen Account? <a href="/User/register"> Registrieren.</a></p>
   <form method="post" class="rt-register">
-    <label class="text-muted" for="rt-email">Email: </label><input value="<?= $email ?>" placeholder="Email-Addresse" class="form-control" id="rt-email" name="rt-email" type="email"><br>
+    <label class="text-muted" for="rt-email">Email: </label><input value="<?= $email ?>" placeholder="Email-Adresse" class="form-control" id="rt-email" name="rt-email" type="email"><br>
     <label class="text-muted" for="rt-password">Passwort: </label><input placeholder="Passwort" class="form-control" id="rt-password" name="rt-password" type="password" onchange="pwLength()"><br>
     <div class="rt-validation"><?= $loginSummary ?></div>
     <input class="rt-btn btn btn-primary" type="submit" value="Los gehts!">
--- view/user_register.php
+++ view/user_register.php
@@ -2,7 +2,7 @@
   <h1 class="jumbotron-heading">REGISTRIEREN</h1>
   <p class="lead text-muted">Registriere dich und geniesse Vorteile wie die Kommentarfunktion!</p>
   <form method="post" class="rt-register">
-    <label class="text-muted" for="rt-email">Email: </label><input value="<?= $email ?>" placeholder="Email-Addresse" class="form-control" id="rt-email" name="rt-email" type="email" onchange="emLength()"><br>
+    <label class="text-muted" for="rt-email">Email: </label><input value="<?= $email ?>" placeholder="Email-Adresse" class="form-control" id="rt-email" name="rt-email" type="email" onchange="emLength()"><br>
     <div class="rt-validation"><?= $emailValidationMessage ?></div>
     <label class="text-muted" for="rt-password">Passwort: </label><input placeholder="Passwort" class="form-control" id="rt-password" name="rt-password" type="password" onchange="pwLength()"><br>
     <div class="rt-validation"><?= $passwordValidationMessage ?></div>
