71db871389aa87a94667c9e4dec0ade74376cdfc
yvo2 <bhofey@bbc-be.local>
2017-10-30 10:49:06 +0100

    + footer cleanup

--- view/footer.php
+++ view/footer.php
@@ -6,7 +6,7 @@
       <!--<p class="float-right">
         <a href="#">Back to top</a>
       </p>-->
-      <div class="footercontent">&copy; by Yvo Hofer and Lars Bärtschi. Powered by <a target="_blank" href="http://transport.opendata.ch">OpenData.ch</a>.<br> Diese Seite steht in keiner Beziehung mit der Schweizerischen Bundesbahn (SBB) oder einem anderen Transportunternehmen. <br><a href="/impressum" target="_blank">Impressum</a></div>
+      <div class="footercontent">&copy; by Yvo Hofer and Lars Bärtschi <br> Powered by <a target="_blank" href="http://transport.opendata.ch">OpenData.ch</a>.<br> Diese Seite steht in keiner Beziehung mit der Schweizerischen Bundesbahn (SBB) oder einem anderen Transportunternehmen. <br><a href="/impressum" target="_blank">Impressum</a></div>
     </div>
   </footer>
   <script src="/js/jquery-3.2.1.min.js"></script>
