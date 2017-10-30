4d056731cc71d19b5e684b816dd79ba76abad715
yvo2 <bhofey@bbc-be.local>
2017-10-30 10:46:28 +0100

    + fixed footer / general cleanup

--- view/exkursion_index.php
+++ view/exkursion_index.php
@@ -1,19 +1,23 @@
-<?php
+<section class="searchcontainer">
+  <div class="container no-limits">
+    <?php
 
-if (count($exkursionen) == 0) {
-  ?>
-    <h4>Keine bewerteten Exkursionen gefunden. Versuche es später erneut! :C</h4>
-  <?php
-}
+    if (count($exkursionen) == 0) {
+      ?>
+        <h4>Keine bewerteten Exkursionen gefunden. Versuche es später erneut! :C</h4>
+      <?php
+    }
 
-foreach ($exkursionen as $exkursion) {
-  ?>
-  <a href="/Route/detail?route=<?= $exkursion->route ?>">
-    <div class="card">
-      <p class="cardtext">Homebase: <?= explode(':', $exkursion->route)[0]; ?><br></p>
-      <p class="cardtext">Umsteigeorte: <?= explode(':', $exkursion->route)[1]; ?></p>
-    </div>
-  </a>
-  <?php
-}
-?>
+    foreach ($exkursionen as $exkursion) {
+      ?>
+      <a href="/Route/detail?route=<?= $exkursion->route ?>">
+        <div class="card">
+          <p class="cardtext">Homebase: <?= explode(':', $exkursion->route)[0]; ?><br></p>
+          <p class="cardtext">Umsteigeorte: <?= explode(':', $exkursion->route)[1]; ?></p>
+        </div>
+      </a>
+      <?php
+    }
+    ?>
+  </div>
+</section>
--- view/route_detail.php
+++ view/route_detail.php
@@ -1,49 +1,53 @@
-<?php require_once "../model/Connection.php"; ?>
-<article class="hreview open special">
-	<table class="table">
-    <tr>
-      <th>Home</th>
-      <td><?= $homebase ?>
-    </tr>
-		<tr>
-			<th>Via</th>
-			<td><?= $via ?>
-		</tr>
-		<?php if (strlen($duration) != 0) { ?>
-			<tr>
-				<th>Dauer</th>
-				<td><?= Connection::getDurationFormatted($duration) ?>
-			</tr>
-		<?php } ?>
+<section class="searchcontainer">
+  <div class="container no-limits">
+		<?php require_once "../model/Connection.php"; ?>
+		<article class="hreview open special">
+			<table class="table">
+		    <tr>
+		      <th>Home</th>
+		      <td><?= $homebase ?>
+		    </tr>
+				<tr>
+					<th>Via</th>
+					<td><?= $via ?>
+				</tr>
+				<?php if (strlen($duration) != 0) { ?>
+					<tr>
+						<th>Dauer</th>
+						<td><?= Connection::getDurationFormatted($duration) ?>
+					</tr>
+				<?php } ?>
 
-	</table>
-  <?php if ($user->signedIn) { ?>
-    <h5>Erfasse einen Kommentar:</h5>
-    <form action="/Route/create" method="post">
-      <textarea class="form-control" name="rt-comment" rows="8" id="createtext" onchange="cmdcreateCheck()"></textarea>
-      <input type="hidden" value="<?= $route ?>" name="rt-route">
-			<div class="rt-validation" id="createerrortext">
-			<?= $validationerror ?>
-		</div>
-      <input class="btn btn-primary rt-btn" type="submit" id="buttonlower">
-    </form>
+			</table>
+		  <?php if ($user->signedIn) { ?>
+		    <h5>Erfasse einen Kommentar:</h5>
+		    <form action="/Route/create" method="post">
+		      <textarea class="form-control" name="rt-comment" rows="8" id="createtext" onchange="cmdcreateCheck()"></textarea>
+		      <input type="hidden" value="<?= $route ?>" name="rt-route">
+					<div class="rt-validation" id="createerrortext">
+					<?= $validationerror ?>
+				</div>
+		      <input class="btn btn-primary rt-btn" type="submit" id="buttonlower">
+		    </form>
 
-  <?php } else { ?>
-    <h5 class="signoninfo"><a href="/User/login">Anmelden</a>, um einen Kommentar zu erfassen.</h5>
-  <?php }
+		  <?php } else { ?>
+		    <h5 class="signoninfo"><a href="/User/login">Anmelden</a>, um einen Kommentar zu erfassen.</h5>
+		  <?php }
 
-		while ($row = $comments->fetch_object()) {
-			?>
-			<div class="comment">
-				<?= $row->email ?> schrieb:<br><br>
-				<?= str_replace("\n", "<br>", $row->bewertung) ?>
-				<?php if($user->email == $row->email) {
+				while ($row = $comments->fetch_object()) {
 					?>
-						<hr><a href="/Comment/edit?id=<?= $row->id ?>">Bearbeiten</a> | <a href="/Comment/delete?id=<?= $row->id ?>">Löschen</a>
+					<div class="comment">
+						<?= $row->email ?> schrieb:<br><br>
+						<?= str_replace("\n", "<br>", $row->bewertung) ?>
+						<?php if($user->signedIn && $user->email == $row->email) {
+							?>
+								<hr><a href="/Comment/edit?id=<?= $row->id ?>">Bearbeiten</a> | <a href="/Comment/delete?id=<?= $row->id ?>">Löschen</a>
+							<?php
+						} ?>
+					</div>
 					<?php
-				} ?>
-			</div>
-			<?php
-		}
-	?>
-</article>
+				}
+			?>
+		</article>
+	</div>
+</section>
