fa02e0cd8506604042a6acf0bd7d221c1b726f23
Bärtschi Lars <bbaerl@bbc-be.local>
2017-10-30 10:11:00 +0100

    + Removed animation due to bugs

--- public/js/script.js
+++ public/js/script.js
@@ -106,17 +106,19 @@ function cmdeditCheck() {
 }
 
 function onSearchButtonMouseOver() {
-  $("#mainbutton").children().fadeOut(100, function() {
+  /* $("#mainbutton").children().fadeOut(100, function() {
     $("#mainbutton").html("<span>Los geht's!</span>");
     $("#mainbutton").children().hide();
     $("#mainbutton").children().fadeIn(100);
-  });
+  }); */
+  $("#mainbutton").html("<span>Los geht's!</span>");
 }
 
 function onSearchButtonMouseOut() {
-  $("#mainbutton").children().fadeOut(100, function() {
+  /* $("#mainbutton").children().fadeOut(100, function() {
     $("#mainbutton").html('<i class="fa fa-subway" aria-hidden="true"></i>');
     $("#mainbutton").children().hide();
     $("#mainbutton").children().fadeIn(100);
-  });
+  }); */
+  $("#mainbutton").html('<i class="fa fa-subway" aria-hidden="true"></i>');
 }
