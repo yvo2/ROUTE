6ccb4c6b59abeeebfc0984c2ec59d575d4ae8427
Bärtschi Lars <bbaerl@bbc-be.local>
2017-10-30 13:31:39 +0100

    + Added deploy information

--- config/config.dist.php
+++ config/config.dist.php
@@ -4,6 +4,7 @@ global $config;
 // In der Variable $config werden alle Daten in einem Array gespeichert, welche für Benutzer- und Routenhandling wichtg sind.
 $config = array(
   "endpoint_url"  => 'http://transport.opendata.ch/v1/connections',
+  "deploy"    => 'live',
 
   "database" => array(
     "host"      => 'localhost:3307',
--- view/footer.php
+++ view/footer.php
@@ -3,10 +3,16 @@
 
   <footer class="text-muted">
     <div class="footercontainer">
-      <!--<p class="float-right">
-        <a href="#">Back to top</a>
-      </p>-->
-      <div class="footercontent">&copy; by Yvo Hofer and Lars Bärtschi <br> Powered by <a target="_blank" href="http://transport.opendata.ch">OpenData.ch</a><br> Diese Seite steht in keiner Beziehung mit der Schweizerischen Bundesbahn (SBB) oder einem anderen Transportunternehmen. <br><a href="/impressum" target="_blank">Impressum</a></div>
+      <?php
+        require_once '../config/config.php';
+        global $config;
+       ?>
+      <div class="footercontent">
+        &copy; by Yvo Hofer and Lars Bärtschi <br> Powered by <a target="_blank" href="http://transport.opendata.ch">OpenData.ch</a><br>
+        Diese Seite steht in keiner Beziehung mit der Schweizerischen Bundesbahn (SBB) oder einem anderen Transportunternehmen. <br>
+        <a href="/impressum" target="_blank">Impressum</a><br>
+        Letzte Aktualisierung: <strong><?= $config["deploy"] ?></strong>
+      </div>
     </div>
   </footer>
   <script src="/js/jquery-3.2.1.min.js"></script>
