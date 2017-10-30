749142c5aa04f1e61f5bffdbc8c405e64e74681a
yvo2 <bhofey@bbc-be.local>
2017-10-30 09:15:47 +0100

    + added Responsiveness for small screens

--- view/default_index.php
+++ view/default_index.php
@@ -4,11 +4,11 @@
     <p class="lead text-muted">Finde deine Exkursion!</p>
     <form method="post" class="rt-search">
       <label class="text-muted" for="rt-homebase">Homebase: </label><input value="<?= $homebase ?>" placeholder="z.B. Bern" class="form-control" id="rt-homebase" name="rt-homebase" type="text"><br>
-      <label class="text-muted" for="rt-via0">Welche Destinationen möchtest du besuchen? </label><input value="<?= $via[0] ?>" placeholder="z.B. Locarno" class="form-control" id="rt-via0" name="rt-via0" type="text" onfocus="focusEvent(0)" onblur="blurEvent(0)"><br>
-      <input value="<?= $via[1] ?>" placeholder="Weitere Destination..." class="form-control" id="rt-via1" name="rt-via1" type="text" onfocus="focusEvent(1)" onblur="blurEvent(1)"><br>
-      <input value="<?= $via[2] ?>" placeholder="Weitere Destination..." class="form-control" id="rt-via2" name="rt-via2" type="text" onfocus="focusEvent(2)" onblur="blurEvent(2)"><br>
-      <input value="<?= $via[3] ?>" placeholder="Weitere Destination..." class="form-control" id="rt-via3" name="rt-via3" type="text" onfocus="focusEvent(3)" onblur="blurEvent(3)"><br>
-      <input value="<?= $via[4] ?>" placeholder="Weitere Destination..." class="form-control" id="rt-via4" name="rt-via4" type="text" onfocus="focusEvent(4)" onblur="blurEvent(4)"><br>
+      <label class="text-muted" for="rt-via0">Welche Destinationen möchtest du besuchen? </label><input value="<?= $via[0] ?>" placeholder="z.B. Locarno" class="form-control viainput" id="rt-via0" name="rt-via0" type="text" onfocus="focusEvent(0)" onblur="blurEvent(0)">
+      <input value="<?= $via[1] ?>" placeholder="Weitere Destination..." class="form-control viainput" id="rt-via1" name="rt-via1" type="text" onfocus="focusEvent(1)" onblur="blurEvent(1)">
+      <input value="<?= $via[2] ?>" placeholder="Weitere Destination..." class="form-control viainput" id="rt-via2" name="rt-via2" type="text" onfocus="focusEvent(2)" onblur="blurEvent(2)">
+      <input value="<?= $via[3] ?>" placeholder="Weitere Destination..." class="form-control viainput" id="rt-via3" name="rt-via3" type="text" onfocus="focusEvent(3)" onblur="blurEvent(3)">
+      <input value="<?= $via[4] ?>" placeholder="Weitere Destination..." class="form-control viainput" id="rt-via4" name="rt-via4" type="text" onfocus="focusEvent(4)" onblur="blurEvent(4)">
       <button class="btn btn-primary rt-btn stretch" id="mainbutton" type="submit"><i class="fa fa-subway" aria-hidden="true"></i></button>
     </form>
   </div>
